bf043209a12bca3c702dab8419e4456423f1b4df
qmatejka <qmister.k@gmail.com>
2016-06-17 12:21:57 +0200

    17/05 10h43

--- src/spaceinvader/model/SpaceInvader.java
+++ src/spaceinvader/model/SpaceInvader.java
@@ -6,7 +6,11 @@
 package spaceinvader.model;
 
 import java.awt.Dimension;
+import java.awt.Point;
 import java.awt.Rectangle;
+import static java.lang.Thread.sleep;
+import spaceinvader.controler.ShipControler;
+import spaceinvader.view.GameView;
 
 /**
  *
@@ -14,20 +18,49 @@ import java.awt.Rectangle;
  */
 public class SpaceInvader {
     
-    public static final boolean isDebugOn = true;
-    private static int gameTime = 5;
+    public static final boolean isDebugOn = false;
+    private static int gameTime = 6;
+    private static String image = "/image/img.png";
     
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws InterruptedException {
-        Projectile P = new Projectile("fire", new Trajectory(10, 0, 0), new Rectangle(10, 10, 4, 12), new Dimension(500, 1000));
-        
+    public static void main(String[] args) throws InterruptedException {        
+        Projectile P2 = new Projectile(new StraightMove(), new Rectangle(10, 10, 4, 12), new Dimension(500, 1000));
+        /*
         while(gameTime>0){
-            P.react();
-            Thread.sleep(1000);
+            if(gameTime==2)
+                P2.getMoves().invertTrajectory();
+            P2.react();
+            Thread.sleep(400);
             gameTime--;
         }
+        */
+        Dimension ground=new Dimension(1280,720);
+        
+        Projectile P = new Projectile(new AlternativeMove(), new Rectangle(10, 10, 4, 12), new Dimension(10, 10));
+        P.setImagePath("src/spaceinvader/view/classic_projectile.png");
+        
+        Spaceship ship = new Spaceship(ground);
+        ship.setImagePath("src/spaceinvader/view/classic_ship.png");
+        ship.setPosition(new Point(250, 680));
+        
+        
+        GameLevel gameLevel = new GameLevel(ground);
+        gameLevel.addGameElementToList(ship);
+        
+        ShipControler sc = new ShipControler(ship, gameLevel);
+        
+        GameView gv = new GameView(sc, gameLevel);
+        
+        
+        while(true){
+            //System.out.println(ship.getPosition());
+            sc.update();
+            gameLevel.makeGameElementsReact();
+            gv.update();
+            sleep(1000/60);
+        }
     }
-    
+
 }
